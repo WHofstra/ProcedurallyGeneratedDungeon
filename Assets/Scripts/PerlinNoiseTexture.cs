b49cd0e889db4a7765274947a94ae4802db11768
Wayne Hofstra <wayne.hofstra@hva.nl>
2020-11-05 13:07:08 +0100

    Project Initialize

---
+++ Assets/Scripts/PerlinNoiseTexture.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class PerlinNoiseTexture : MonoBehaviour
+{
+    [SerializeField] int _pixWidth;
+    [SerializeField] int _pixHeight;
+
+    Texture2D texture;
+    Color[] color;
+    Renderer rendr;
+
+    Vector2 originPoint = new Vector2(0, 0);
+    float scale = 1f;
+
+    void Start()
+    {
+        rendr = GetComponent<Renderer>();
+
+        texture = new Texture2D(_pixWidth, _pixHeight);
+        color = new Color[texture.width * texture.height];
+        rendr.material.mainTexture = texture;
+
+        CalculateNoise();
+    }
+
+    void CalculateNoise()
+    {
+        float y = 0f;
+
+        while (y < texture.height)
+        {
+            float x = 0f;
+            while (x < texture.width)
+            {
+                float xCoord = originPoint.x + x / texture.width * scale;
+                float yCoord = originPoint.y + y / texture.height * scale;
+                float sample = Mathf.PerlinNoise(xCoord, yCoord);
+                color[(int)y * texture.width + (int)x] = new Color(sample, sample, sample);
+                x++;
+            }
+            y++;
+        }
+
+        texture.SetPixels(color);
+        texture.Apply();
+    }
+}
---
+++ Assets/Scripts/TilePlacement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TilePlacement : MonoBehaviour
+{
+    [SerializeField] Tile _tile;
+
+    Tilemap tileMap;
+    //Vector3Int pos;
+
+    void Start()
+    {
+        tileMap = GetComponent<Tilemap>();
+        //pos = new Vector3Int(0, 0, 0);
+
+        //tileMap.SetTile(pos, _tile);
+    }
+}
