db79c711e22734c9a0ae5346a26233c63f482c3a
Wayne Hofstra <wayne.hofstra@hva.nl>
2020-11-08 16:45:02 +0100

    Added Multiple Center Points and More Sorting

--- Assets/Scripts/TilePlacement.cs
+++ Assets/Scripts/TilePlacement.cs
@@ -18,8 +18,8 @@ public class TilePlacement : MonoBehaviour
     Tilemap tileMap;
     PerlinNoiseTexture texture;
 
-    List<Vector2Int> centerPoint = new List<Vector2Int>();
-    Vector3Int[] tileNeighbor    = new Vector3Int[2];
+    List<Vector2Int>[] centerPoint = new List<Vector2Int>[2];
+    Vector3Int[] tileNeighbor      = new Vector3Int[2];
     Vector3Int tilePos;
     Vector2Int gridSize;
 
@@ -28,6 +28,9 @@ public class TilePlacement : MonoBehaviour
         tileMap = GetComponent<Tilemap>();
         texture = GetComponent<PerlinNoiseTexture>();
         grid    = FindObjectOfType<Grid>();
+
+        centerPoint[0] = new List<Vector2Int>();
+        centerPoint[1] = new List<Vector2Int>();
 
         if (grid != null) {
             grid = grid.GetComponent<Grid>();
@@ -66,8 +69,10 @@ public class TilePlacement : MonoBehaviour
                     {
                         tilePos = new Vector3Int(i - Mathf.RoundToInt(gridSize.x / 2f) + Mathf.FloorToInt(grid.cellSize.x * _pixelSize.x * 0.5f),
                                                -(j - Mathf.RoundToInt(gridSize.y / 2f) + Mathf.FloorToInt(grid.cellSize.y * _pixelSize.y * 0.5f)), 0);
+
+                        //Highlight Center of Room
                         tileMap.SetTile(tilePos, otherTile);
-                        centerPoint.Add(new Vector2Int(tilePos.x, tilePos.y));
+                        centerPoint[0].Add(new Vector2Int(tilePos.x, tilePos.y));
                     }
 
                     tilePos = new Vector3Int(i - Mathf.RoundToInt(gridSize.x / 2f),
@@ -81,24 +86,52 @@ public class TilePlacement : MonoBehaviour
             }
         }
 
-        //Draw Connecting Bridges
-        if (centerPoint.Count > 0) {
-            for (int i = 0; i < (centerPoint.Count - 1); i++) {
-                PlaceHorizontalBridges(centerPoint[i], centerPoint[i + 1], aTile);
+        if (centerPoint[0].Count > 0)
+        {
+            //Find Extra Center Points in Bigger Rooms
+            centerPoint[0] = AddExtraPoints(centerPoint[0], otherTile);
+            centerPoint[0] = AddExtraPoints(centerPoint[0], otherTile);
+
+            //Draw Connecting Bridges
+            centerPoint[0] = SortOnYValues(centerPoint[0]);
+            for (int i = 0; i < (centerPoint[0].Count - 1); i++)
+            {
+                PlaceHorizontalBridges(centerPoint[0][i], centerPoint[0][i + 1], aTile);
             }
 
-            centerPoint = SortOnXValues(centerPoint);
-
             /* //To Check Sort in Debug
-            foreach (Vector2Int point in centerPoint) {
+            foreach (Vector2Int point in centerPoint[0]) {
                 Debug.Log(point);
             }//*/
 
-            //*
-            for (int i = (centerPoint.Count - 1); i > 0; i--) {
-                PlaceVerticalBridges(centerPoint[i], centerPoint[i - 1], aTile);
-            }//*/
+            centerPoint[0] = SortOnXValues(centerPoint[0]);
+            for (int i = (centerPoint[0].Count - 1); i > 0; i--)
+            {
+                PlaceVerticalBridges(centerPoint[0][i], centerPoint[0][i - 1], aTile);
+            }
         }
+    }
+
+    List<Vector2Int> AddExtraPoints(List<Vector2Int> centers, Tile aTile)
+    {
+        List<Vector2Int> newList = new List<Vector2Int>();
+
+        foreach (Vector2Int point in centers)
+        {
+            tileNeighbor[0] = new Vector3Int(point.x + Mathf.FloorToInt(grid.cellSize.x * _pixelSize.x), point.y, 0);
+            tileNeighbor[1] = new Vector3Int(point.x, point.y - Mathf.FloorToInt(grid.cellSize.y * _pixelSize.y), 0);
+
+            for (int i = 0; i < 2; i++) {
+                if (tileMap.GetTile(tileNeighbor[i]) != null && tileMap.GetTile(tileNeighbor[i]) != aTile)
+                {
+                    tileMap.SetTile(tileNeighbor[i], aTile);
+                    newList.Add(new Vector2Int(tileNeighbor[i].x, tileNeighbor[i].y));
+                }
+            }
+            newList.Add(point);
+        }
+
+        return newList;
     }
 
     void PlaceHorizontalBridges(Vector2Int posA, Vector2Int posB, Tile aTile)
@@ -167,6 +200,34 @@ public class TilePlacement : MonoBehaviour
         }
 
         return aSize;
+    }
+
+    List<Vector2Int> SortOnYValues(List<Vector2Int> currentList)
+    {
+        Vector2Int[] sorter = currentList.ToArray();
+
+        //Selection Sorting
+        for (int i = 0; i < sorter.Length; i++)
+        {
+            int minimumIndex = i;
+            for (var j = i + 1; j < sorter.Length; j++)
+            {
+                if (sorter[minimumIndex].y > sorter[j].y ||
+                   (sorter[minimumIndex].y == sorter[j].y && sorter[minimumIndex].x > sorter[j].x))
+                {
+                    minimumIndex = j;
+                }
+            }
+
+            if (minimumIndex != i)
+            {
+                Vector2Int temp = sorter[minimumIndex];
+                sorter[minimumIndex] = sorter[i];
+                sorter[i] = temp;
+            }
+        }
+
+        return sorter.ToList<Vector2Int>();
     }
 
     List<Vector2Int> SortOnXValues(List<Vector2Int> currentList)
